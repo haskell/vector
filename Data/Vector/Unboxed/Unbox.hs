6c76dd471ecc8746e30852f79ec855fdebab52bb
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2008-07-06 15:07:25 +0000

    Unbox instances for Float and Double

--- Data/Vector/Unboxed/Unbox.hs
+++ Data/Vector/Unboxed/Unbox.hs
@@ -7,14 +7,19 @@ module Data.Vector.Unboxed.Unbox (
 import GHC.Base (
     Int(..)
   )
+import GHC.Float (
+    Float(..), Double(..)
+  )
 
 import GHC.Prim (
     ByteArray#, MutableByteArray#, State#,
 
-    Int#, indexIntArray#, readIntArray#, writeIntArray#
+    Int#, indexIntArray#,    readIntArray#,    writeIntArray#,
+          indexFloatArray#,  readFloatArray#,  writeFloatArray#,
+          indexDoubleArray#, readDoubleArray#, writeDoubleArray#
   )
 import Data.Array.Base (
-    wORD_SCALE
+    wORD_SCALE, fLOAT_SCALE, dOUBLE_SCALE
   )
 
 class Unbox a where
@@ -29,4 +34,18 @@ instance Unbox Int where
   read#  arr# i# s#         = case readIntArray# arr# i# s# of
                                 (# s1#, n# #) -> (# s1#, I# n# #)
   write# arr# i# (I# n#) s# = writeIntArray# arr# i# n# s#
+
+instance Unbox Float where
+  size#  _                  = fLOAT_SCALE
+  at#    arr# i#            = F# (indexFloatArray# arr# i#)
+  read#  arr# i# s#         = case readFloatArray# arr# i# s# of
+                                (# s1#, x# #) -> (# s1#, F# x# #)
+  write# arr# i# (F# x#) s# = writeFloatArray# arr# i# x# s#
+
+instance Unbox Double where
+  size#  _                  = dOUBLE_SCALE
+  at#    arr# i#            = D# (indexDoubleArray# arr# i#)
+  read#  arr# i# s#         = case readDoubleArray# arr# i# s# of
+                                (# s1#, x# #) -> (# s1#, D# x# #)
+  write# arr# i# (D# x#) s# = writeDoubleArray# arr# i# x# s#
 
