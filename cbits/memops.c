7e95e94c5508bcfbc2979213737d7c680511b593
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2010-04-04 12:57:04 +0000

    Nuke cbits

--- cbits/memops.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-
-void memcpy_off( char *dst, int doff, char *src, int soff, int len )
-{
-  memcpy( dst + doff, src + soff, len );
-}
-
--- cbits/memops.h
+++
@@ -1,7 +0,0 @@
-#ifndef haskell_vector_memops_h
-#define haskell_vector_memops_h
-
-void memcpy_off( char *dst, int doff, char *src, int soff, int len );
-
-#endif
-
