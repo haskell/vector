8b8faedc8b4e1f81715b8678f78a709b59f1c0fd
Herbert Valerio Riedel <hvr@gnu.org>
2014-11-23 15:55:12 +0100

    Add support for `deepseq-1.4.0.0`
    See haskell/deepseq#1 for more details
    
    This also bumps the package version to 0.10.12.2

--- Data/Vector/Unboxed/Base.hs
+++ Data/Vector/Unboxed/Base.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
+{-# LANGUAGE BangPatterns, MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
 #if __GLASGOW_HASKELL__ >= 707
 {-# LANGUAGE DeriveDataTypeable, StandaloneDeriving #-}
 #endif
@@ -25,7 +25,7 @@ import qualified Data.Vector.Generic.Mutable as M
 
 import qualified Data.Vector.Primitive as P
 
-import Control.DeepSeq ( NFData )
+import Control.DeepSeq ( NFData(rnf) )
 
 import Control.Monad.Primitive
 import Control.Monad ( liftM )
@@ -60,8 +60,8 @@ type instance G.Mutable Vector = MVector
 
 class (G.Vector Vector a, M.MVector MVector a) => Unbox a
 
-instance NFData (Vector a)
-instance NFData (MVector s a)
+instance NFData (Vector a) where rnf !_ = ()
+instance NFData (MVector s a) where rnf !_ = ()
 
 -- -----------------
 -- Data and Typeable
