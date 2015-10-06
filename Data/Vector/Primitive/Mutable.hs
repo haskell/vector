bcf2bac68c1ebc56e158dcb226414602aa4b22a1
Justin Raymond <justin.raymond.14@gmail.com>
2015-10-06 19:25:16 -0400

    update docs for Data.Vector.Primitive.unsafeNew

--- Data/Vector/Primitive/Mutable.hs
+++ Data/Vector/Primitive/Mutable.hs
@@ -211,7 +211,7 @@ new :: (PrimMonad m, Prim a) => Int -> m (MVector (PrimState m) a)
 {-# INLINE new #-}
 new = G.new
 
--- | Create a mutable vector of the given length. The length is not checked.
+-- | Create a mutable vector of the given length. The memory is not initialized.
 unsafeNew :: (PrimMonad m, Prim a) => Int -> m (MVector (PrimState m) a)
 {-# INLINE unsafeNew #-}
 unsafeNew = G.unsafeNew
