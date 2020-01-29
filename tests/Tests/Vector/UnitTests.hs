6e9e0a23dd7838c29e3710a6842dacc019105df5
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-01-29 19:02:11 +0300

    Add regression test for #188
    Test do reproduce the bug

--- tests/Tests/Vector/UnitTests.hs
+++ tests/Tests/Vector/UnitTests.hs
@@ -5,15 +5,19 @@ module Tests.Vector.UnitTests (tests) where
 
 import Control.Applicative as Applicative
 import Control.Monad.Primitive
+import Data.Int
+import Data.Word
+import Data.Typeable
 import qualified Data.Vector.Generic  as Generic
 import qualified Data.Vector.Storable as Storable
+import qualified Data.Vector          as Vector
 import Foreign.Ptr
 import Foreign.Storable
 import Text.Printf
 
 import Test.Framework
 import Test.Framework.Providers.HUnit (testCase)
-import Test.HUnit (Assertion, assertBool)
+import Test.HUnit (Assertion, assertBool, (@=?))
 
 newtype Aligned a = Aligned { getAligned :: a }
 
@@ -42,7 +46,29 @@ tests =
       , testCase "Aligned Int" $
           checkAddressAlignment alignedIntVec
       ]
+  , testGroup "Regression tests"
+    [ testGroup "enumFromTo crash #188" $
+      [ regression188 (Proxy :: Proxy Word8)
+      , regression188 (Proxy :: Proxy Word16)
+      , regression188 (Proxy :: Proxy Word32)
+      , regression188 (Proxy :: Proxy Word64)
+      , regression188 (Proxy :: Proxy Word)
+      , regression188 (Proxy :: Proxy Int8)
+      , regression188 (Proxy :: Proxy Int16)
+      , regression188 (Proxy :: Proxy Int32)
+      , regression188 (Proxy :: Proxy Int64)
+      , regression188 (Proxy :: Proxy Int)
+      , regression188 (Proxy :: Proxy Char)
+      ]
+    ]
   ]
+
+regression188
+  :: forall a. (Typeable a, Enum a, Bounded a, Eq a, Show a)
+  => Proxy a -> Test
+regression188 _ = testCase (show (typeOf (undefined :: a)))
+  $ Vector.fromList [maxBound::a] @=? Vector.enumFromTo maxBound maxBound
+{-# INLINE regression188 #-}
 
 alignedDoubleVec :: Storable.Vector (Aligned Double)
 alignedDoubleVec = Storable.fromList $ map Aligned [1, 2, 3, 4, 5]
