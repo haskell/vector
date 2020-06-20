3b989ff4dcca3c9e2f1ba85e224d93db55709ea7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-06-20 22:38:24 +0300

    Again there're tests

--- tests/Tests/Vector/Property.hs
+++ tests/Tests/Vector/Property.hs
@@ -162,8 +162,8 @@ testPolymorphicFunctions _ = $(testProperties [
         'prop_imapM, 'prop_imapM_,
 
         -- Zipping
-        'prop_zipWith, 'prop_zipWith3, {- ... -}
-        'prop_izipWith, 'prop_izipWith3, {- ... -}
+        'prop_zipWith, 'prop_zipWith3,
+        'prop_izipWith, 'prop_izipWith3,
         'prop_izipWithM, 'prop_izipWithM_,
 
         -- Monadic zipping
@@ -193,9 +193,6 @@ testPolymorphicFunctions _ = $(testProperties [
 
         -- Specialised folds
         'prop_all, 'prop_any,
-
-        -- Monadic folds
-        {- ... -}
 
         -- Scans
         'prop_prescanl, 'prop_prescanl',
