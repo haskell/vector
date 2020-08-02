b15027f542765fce3331b904d6492d0ccdf87127
Genki Sato <13384803+gksato@users.noreply.github.com>
2020-08-02 14:38:57 +0900

    make dropWhile fusion adaptive
    Now dropWhile's implementation uses stream
    only in case the stream fusion is already running.
    This is expected to reduce unnecessary re-allocation.
    
    cf. https://github.com/haskell/vector/issues/141
    https://github.com/haskell/vector/issues/108

--- Data/Vector/Generic.hs
+++ Data/Vector/Generic.hs
@@ -1381,8 +1381,19 @@ takeWhile f = unstream . Bundle.takeWhile f . stream
 -- | /O(n)/ Drop the longest prefix of elements that satisfy the predicate
 -- without copying.
 dropWhile :: Vector v a => (a -> Bool) -> v a -> v a
-{-# INLINE dropWhile #-}
-dropWhile f = unstream . Bundle.dropWhile f . stream
+{-# INLINE_FUSED dropWhile #-}
+-- In the case that the argument is an actual vector,
+-- this is a faster solution than stream fusion.
+dropWhile f xs = case findIndex (not . f) xs of
+                   Just i  -> unsafeDrop i xs
+                   Nothing -> empty
+
+-- If the argument to 'dropWhile' comes from a stream,
+-- we need to avoid unnecessary allocation.
+{-# RULES
+"dropWhile/unstream [Vector]" forall f p.
+  dropWhile f (new (New.unstream p)) = new (New.unstream (Bundle.dropWhile f p))
+  #-}
 
 -- Parititioning
 -- -------------
