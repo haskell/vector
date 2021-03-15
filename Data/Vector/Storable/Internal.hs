25a09a7f14f0cee21852673fc8cfabecdf52b67d
Ben Gamari <ben@smart-cactus.org>
2021-03-15 10:41:25 -0400

    Use unsafeWithForeignPtr where possible
    In GHC 9.0.1 `withForeignPtr` became (necessarily) considerably more
    expensive. In GHC ticket #19474, it was noted that `Storable` `Vector`s
    are affected by this regression. Fix this by using
    `unsafeWithForeignPtr` when it is known that the continuation is
    not divergent.
    
    See also GHC #17760.

--- Data/Vector/Storable/Internal.hs
+++ Data/Vector/Storable/Internal.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 -- |
 -- Module      : Data.Vector.Storable.Internal
 -- Copyright   : (c) Roman Leshchinskiy 2009-2010
@@ -11,12 +13,17 @@
 --
 
 module Data.Vector.Storable.Internal (
-  getPtr, setPtr, updPtr
+  getPtr, setPtr, updPtr, unsafeWithForeignPtr
 ) where
 
 import Foreign.ForeignPtr ()
 import Foreign.Ptr        ()
 import GHC.ForeignPtr   ( ForeignPtr(..) )
+#if MIN_VERSION_base(4,15,0)
+import GHC.ForeignPtr       ( unsafeWithForeignPtr )
+#else
+import Foreign.ForeignPtr   ( withForeignPtr )
+#endif
 import GHC.Ptr          ( Ptr(..) )
 
 getPtr :: ForeignPtr a -> Ptr a
@@ -31,3 +38,12 @@ updPtr :: (Ptr a -> Ptr a) -> ForeignPtr a -> ForeignPtr a
 {-# INLINE updPtr #-}
 updPtr f (ForeignPtr p c) = case f (Ptr p) of { Ptr q -> ForeignPtr q c }
 
+#if !MIN_VERSION_base(4,15,0)
+-- | A compatibility wrapper for 'GHC.ForeignPtr.unsafeWithForeignPtr' provided
+-- by GHC 9.0.1 and later.
+--
+-- Only to be used when the continuation is known not to
+-- unconditionally diverge lest unsoundness can result.
+unsafeWithForeignPtr :: ForeignPtr a -> (Ptr a -> IO b) -> IO b
+unsafeWithForeignPtr = withForeignPtr
+#endif
