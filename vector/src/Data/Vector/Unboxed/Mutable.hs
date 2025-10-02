4e25a8becd49adf64dfbfa1ec58aada0940b54cb
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2025-10-02 16:31:00 +0300

    Prepare reexports for creation of patterns
    for unsafe unboxed vectors

--- vector/src/Data/Vector/Unboxed/Mutable.hs
+++ vector/src/Data/Vector/Unboxed/Mutable.hs
@@ -68,7 +68,8 @@ module Data.Vector.Unboxed.Mutable (
   PrimMonad, PrimState, RealWorld
 ) where
 
-import Data.Vector.Unboxed.Unsafe
+import Data.Vector.Unboxed.Unsafe (MVector, STVector,Unbox,IOVector)
+import qualified Data.Vector.Unboxed.Unsafe as U
 import qualified Data.Vector.Generic.Mutable as G
 import Data.Vector.Fusion.Util ( delayed_min )
 import Control.Monad.Primitive
@@ -671,20 +672,20 @@ ifoldrM' = G.ifoldrM'
 zip :: (Unbox a, Unbox b) => MVector s a ->
                              MVector s b -> MVector s (a, b)
 {-# INLINE_FUSED zip #-}
-zip as bs = MV_2 len (unsafeSlice 0 len as) (unsafeSlice 0 len bs)
+zip as bs = U.MV_2 len (unsafeSlice 0 len as) (unsafeSlice 0 len bs)
   where len = length as `delayed_min` length bs
 -- | /O(1)/ Unzip 2 vectors.
 unzip :: (Unbox a, Unbox b) => MVector s (a, b) -> (MVector s a,
                                                     MVector s b)
 {-# INLINE unzip #-}
-unzip (MV_2 _ as bs) = (as, bs)
+unzip (U.MV_2 _ as bs) = (as, bs)
 
 -- | /O(1)/ Zip 3 vectors.
 zip3 :: (Unbox a, Unbox b, Unbox c) => MVector s a ->
                                        MVector s b ->
                                        MVector s c -> MVector s (a, b, c)
 {-# INLINE_FUSED zip3 #-}
-zip3 as bs cs = MV_3 len (unsafeSlice 0 len as)
+zip3 as bs cs = U.MV_3 len (unsafeSlice 0 len as)
                          (unsafeSlice 0 len bs)
                          (unsafeSlice 0 len cs)
   where
@@ -696,7 +697,7 @@ unzip3 :: (Unbox a,
                                                MVector s b,
                                                MVector s c)
 {-# INLINE unzip3 #-}
-unzip3 (MV_3 _ as bs cs) = (as, bs, cs)
+unzip3 (U.MV_3 _ as bs cs) = (as, bs, cs)
 
 -- | /O(1)/ Zip 4 vectors.
 zip4 :: (Unbox a, Unbox b, Unbox c, Unbox d) => MVector s a ->
@@ -704,7 +705,7 @@ zip4 :: (Unbox a, Unbox b, Unbox c, Unbox d) => MVector s a ->
                                                 MVector s c ->
                                                 MVector s d -> MVector s (a, b, c, d)
 {-# INLINE_FUSED zip4 #-}
-zip4 as bs cs ds = MV_4 len (unsafeSlice 0 len as)
+zip4 as bs cs ds = U.MV_4 len (unsafeSlice 0 len as)
                             (unsafeSlice 0 len bs)
                             (unsafeSlice 0 len cs)
                             (unsafeSlice 0 len ds)
@@ -722,7 +723,7 @@ unzip4 :: (Unbox a,
                                                   MVector s c,
                                                   MVector s d)
 {-# INLINE unzip4 #-}
-unzip4 (MV_4 _ as bs cs ds) = (as, bs, cs, ds)
+unzip4 (U.MV_4 _ as bs cs ds) = (as, bs, cs, ds)
 
 -- | /O(1)/ Zip 5 vectors.
 zip5 :: (Unbox a,
@@ -735,7 +736,7 @@ zip5 :: (Unbox a,
                      MVector s d ->
                      MVector s e -> MVector s (a, b, c, d, e)
 {-# INLINE_FUSED zip5 #-}
-zip5 as bs cs ds es = MV_5 len (unsafeSlice 0 len as)
+zip5 as bs cs ds es = U.MV_5 len (unsafeSlice 0 len as)
                                (unsafeSlice 0 len bs)
                                (unsafeSlice 0 len cs)
                                (unsafeSlice 0 len ds)
@@ -757,7 +758,7 @@ unzip5 :: (Unbox a,
                                                      MVector s d,
                                                      MVector s e)
 {-# INLINE unzip5 #-}
-unzip5 (MV_5 _ as bs cs ds es) = (as, bs, cs, ds, es)
+unzip5 (U.MV_5 _ as bs cs ds es) = (as, bs, cs, ds, es)
 
 -- | /O(1)/ Zip 6 vectors.
 zip6 :: (Unbox a,
@@ -772,7 +773,7 @@ zip6 :: (Unbox a,
                      MVector s e ->
                      MVector s f -> MVector s (a, b, c, d, e, f)
 {-# INLINE_FUSED zip6 #-}
-zip6 as bs cs ds es fs = MV_6 len (unsafeSlice 0 len as)
+zip6 as bs cs ds es fs = U.MV_6 len (unsafeSlice 0 len as)
                                   (unsafeSlice 0 len bs)
                                   (unsafeSlice 0 len cs)
                                   (unsafeSlice 0 len ds)
@@ -798,7 +799,7 @@ unzip6 :: (Unbox a,
                                                         MVector s e,
                                                         MVector s f)
 {-# INLINE unzip6 #-}
-unzip6 (MV_6 _ as bs cs ds es fs) = (as, bs, cs, ds, es, fs)
+unzip6 (U.MV_6 _ as bs cs ds es fs) = (as, bs, cs, ds, es, fs)
 
 -- $setup
 -- >>> import Prelude (Char, (*), ($))
