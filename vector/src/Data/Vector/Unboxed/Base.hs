8cbf15ac7b03cba37b4b2cd06a5746466b622093
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-02-10 14:30:03 +0300

    Restore Data.Vector.Unboxed.Base as deprecated

---
+++ vector/src/Data/Vector/Unboxed/Base.hs
@@ -0,0 +1,6 @@
+{-# OPTIONS_HADDOCK hide #-}
+module Data.Vector.Unboxed.Base {-# DEPRECATED "Use Data.Vector.Unboxed.Unsafe instead" #-}
+  ( module Data.Vector.Unboxed.Unsafe
+  ) where
+
+import Data.Vector.Unboxed.Unsafe
